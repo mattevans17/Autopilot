06e78fc3ab83588fcf24fbc4fc0625d958e05f96
Matt Evans <matveyanov24@gmail.com>
2021-11-27 16:21:02 +0300

    Add classifier

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
 import numpy as np
 import cv2
 
-
 FRAME_SIZE = (640, 480)
-VIDEO_URL = 'media/2.mp4'
+VIDEO_URL = 'media/video.mp4'
 BLUR_MATRIX = (1, 1)
-POINTS_FREQ = 50
-MIN_LANE_WIDTH = 3
+
+CLASSIFIER_POINTS_FREQ = 40
+CLASSIFIER_MIN_LANE_WIDTH = 3
+CLASSIFIER_OFFSET_X_MAX = 50
+CLASSIFIER_CLASS_MAX_SIZE = 20
 
 
 def gauassian_blur(frame):
@@ -46,24 +48,49 @@ def resize_frame(frame):
 
 
 def lane_points(original):
+    classes = []
     frame = np.copy(original)
     frame[frame == 255] = 1
     shape = np.array(frame).shape
-    offset = shape[0] // POINTS_FREQ
+    offset = shape[0] // CLASSIFIER_POINTS_FREQ
     points = []
     for idx in range(shape[0]):
         if idx % offset == 0:
             row = frame[idx]
             matches_num = 0
+
             for idx_1 in range(shape[1]):
                 curr_value = row[idx_1]
                 if curr_value == 1:
                     matches_num += 1
-                elif matches_num >= MIN_LANE_WIDTH:
+
+                elif matches_num >= CLASSIFIER_MIN_LANE_WIDTH:
                     point = (idx_1, idx)
-                    points.append(point)
+
+                    if idx == 0:
+                        classes.append([point])
+
+                    else:
+                        current_include_in_class = False
+                        for class_idx in range(len(classes)):
+                            try:
+                                prev_pt_x = classes[class_idx][(idx // offset) - 1][0]
+
+                            except:
+                                prev_pt_x = classes[class_idx][len(classes[class_idx])-1][0]
+
+                            if abs(point[0] - prev_pt_x) < CLASSIFIER_OFFSET_X_MAX:
+                                classes[class_idx].append(point)
+                                current_include_in_class = True
+                                break
+
+                        if not current_include_in_class:
+                            classes.append([point])
+
                     matches_num = 0
-    return points
+
+    classes = [_class for _class in classes if len(_class) > CLASSIFIER_CLASS_MAX_SIZE]
+    return classes
 
 
 def main():
@@ -79,12 +106,23 @@ def main():
         yw_filtered = yw_filter(blurred)
         top_view = get_top_view(yw_filtered)
         original_top_view = get_top_view(resized)
-        points = lane_points(top_view)
-        for point in points:
-            original_top_view = cv2.circle(original_top_view, point, radius=5, color=(0, 255, 0), thickness=-1)
 
-        cv2.imshow('frame', top_view)
-        cv2.imshow('yw_filtered', original_top_view)
+        pts_classes = lane_points(top_view)
+        mask = np.zeros(original_top_view.shape, dtype=np.uint8)
+        for class_idx in range(len(pts_classes)):
+            for point in pts_classes[class_idx]:
+                color = (0, 0, 255)
+                if class_idx == 0:
+                    color = (0, 255, 0)
+                elif class_idx == 1:
+                    color = (255, 0, 0)
+                mask = cv2.circle(
+                    mask, point, radius=5,
+                    color=color, thickness=-1
+                )
+
+        cv2.imshow('frame', original_top_view)
+        cv2.imshow('mask', mask)
 
     cap.release()
     cv2.destroyAllWindows()
